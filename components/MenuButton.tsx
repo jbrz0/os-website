836aa3f1b6fc772817525d55fa2e89c9a163d2c9
Justin <justinb@live.ca>
2020-06-18 22:53:03 -0400

    nav mobile menu overlay

--- components/MenuButton.tsx
+++ components/MenuButton.tsx
@@ -1,4 +1,7 @@
+import {useState, useEffect} from 'react'
 import Link from 'next/link'
+import {btnItems} from './Nav'
+import {useKeyPress} from '../hooks/useKeyPress'
 
 interface Viewport {
   mobile?: boolean
@@ -7,18 +10,83 @@ const MenuButton: React.FC<Viewport> = ({mobile}) => {
 
   let viewport: string;
   const sm:string = 'block md:hidden'
-  const md:string = 'hidden md:block lg:hidden'
+  const md:string = 'hidden md:block lg:hidden ml-2'
 
   if (mobile) viewport = sm
   else viewport = md
 
-  return (
+  const [show, setShow] = useState<boolean>(false)
+
+  // On Escape key press, close menu
+  const escPress = useKeyPress('Escape')
+  useEffect(() => {
+    if (show) setShow(false)
+  }, [escPress])
+
+  return (<>
     <li className={`bg-gray-700 hover:bg-gray-800 cursor-pointer rounded-sm
-      mr-1 flex self-center py-2 px-2 ml-2 ${viewport}`}>
-      <Link href="/"><a>
-        <img src="icons/menu.svg" alt="Menu" className="w-4" />
+      flex self-center py-2 px-2 ${viewport}`} onClick={() => setShow(true)}>
+      <Link href="/"><a className="w-4">
+        <img src="icons/menu.svg" alt="Menu" />
       </a></Link>
     </li>
+
+    {(show && viewport === md) &&
+      <div className="text-white">TABLET MENU</div>
+    }
+
+    {(show && viewport === sm) &&
+      <div className="fixed left-0 right-0 top-0 w-full h-full
+        z-10 bg-gray-900">
+
+
+      <div className="fixed w-full">
+        <div className="m-5">
+          <ul className="flex justify-between">
+            <li className="w-12 h-12 flex justify-center">
+              <img src="icons/close.svg" alt="Close Menu"
+                onClick={() => setShow(false)}
+                className="w-10 cursor-pointer" />
+            </li>
+            {btnItems.map(({url, icon, title}) => <li
+              className="w-12 h-12 flex justify-center">
+              <img src={icon}className="w-6" /></li>)}
+          </ul>
+        </div>
+      </div>
+
+      <div className="h-full flex items-center">
+        <div className="ml-8 pt-6 pb-2">
+          <div className="menu-text text-4xl text-white mb-6">Home</div>
+          <div className="menu-text text-4xl text-white mb-6">Projects</div>
+          <div className="menu-text text-4xl text-white mb-6">Labs</div>
+          <div className="menu-text text-4xl text-white mb-6">Blog</div>
+          <div className="menu-text text-4xl text-white mb-6">Shop</div>
+        </div>
+      </div>
+
+      <div className="fixed w-full bottom-0">
+        <div className="flex bg-gray-800 flex justify-center py-3">
+          <ul className="flex social-icons">
+            <li className="w-12 h-12 mx-2">
+              <img className="nav-social" src="social/reddit.svg" alt=""/>
+            </li>
+            <li className="w-12 h-12 mx-2">
+              <img className="nav-social" src="social/twitter.svg" alt=""/>
+            </li>
+            <li className="w-12 h-12 mx-2">
+              <img className="nav-social" src="social/github.svg" alt=""/>
+            </li>
+            <li className="w-12 h-12 mx-2">
+              <img className="nav-social" src="social/email.svg" alt=""/>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+    }
+    </>
   )
 }
 
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
-import { useRouter, NextRouter } from 'next/router'
+import ReactTooltip from "react-tooltip"
+import {useRouter, NextRouter} from 'next/router'
 import MenuButton from './MenuButton'
+import {titleCase} from '../helpers/format-text'
 
 interface Content {
   url: string
@@ -17,7 +19,7 @@ const navItems: Content[] = [
   { url: "/shop", icon: 'icons/shop', title: 'Shop' },
 ]
 
-const btnItems: Content[] = [
+export const btnItems: Content[] = [
   { url: "/profile", icon: 'icons/profile.svg', title: 'Profile', isNew: false },
   { url: "/cart", icon: 'icons/cart.svg', title: 'Cart', isNew: true },
   { url: "/chat", icon: 'icons/chat.svg', title: 'Chat', isNew: false },
@@ -43,27 +45,40 @@ const NavItem: React.FC<Content> = ({url, icon, title}) => {
 }
 
 const Nav: React.FC = () => {
-  return <nav
-    className="bg-gray-900 rounded-sm m-1 p-2 grid grid-cols-12 gap-1">
 
-      <div className="md:col-span-5 lg:col-span-3 flex self-center">
+  return <div className="fixed w-full">
+    <nav className="bg-gray-900 rounded-sm m-1 p-2 grid grid-cols-12 gap-1 shadow-lg">
 
-        <ul className="flex top-menu">
+      <ReactTooltip uuid="tooltip" />
+
+      <div className="sm:col-span-12 md:col-span-5 lg:col-span-3 flex self-center">
+
+        <ul className="flex">
           <MenuButton mobile />
         </ul>
 
         <Link href="/"><a className="flex flex-none">
-          <img src="/logo.svg" alt="Odd Scenes Logo" className="w-32 xl:w-40" />
+          <img src="/logo.svg" alt="Odd Scenes Logo"
+            className="w-40 xl:w-40 nav-logo" />
         </a></Link>
 
+        <ul className="absolute right-0 mr-2 md:hidden">
+          <li className={`bg-gray-700 hover:bg-gray-800 cursor-pointer
+            rounded-sm mr-1 flex self-center py-2 px-2`}>
+            <Link href="/"><a className="w-4">
+              <img src="icons/search.svg" alt="Menu" className="nav-search" />
+            </a></Link>
+          </li>
+        </ul>
+
         {/* Optional Badge */}
-        {/* <div className="text-white ml-2
+        {/* <div className="text-white ml-2 hidden md:block
           text-xs bg-blue px-1 rounded-sm nav-badge">
-          This is a message!
+          This is message!
         </div> */}
       </div>
 
-      <div className="md:col-span-7 lg:col-span-5">
+      <div className="hidden md:block md:col-span-7 lg:col-span-5">
         <ul className="flex md:justify-end lg:justify-center top-menu">
           {navItems.map(({url, icon, title}, i) =>
             <NavItem title={title} icon={icon} url={url} key={i} />)}
@@ -77,7 +92,10 @@ const Nav: React.FC = () => {
           <ul className="flex justify-between">
             {btnItems.map(({url, icon, title, isNew}, i) =>
               <li className="bg-gray-700 hover:bg-gray-800 cursor-pointer
-                rounded-sm mr-1 flex flex-none py-1 xl:px-2 lg:px-1" key={i}>
+                rounded-sm mr-1 flex flex-none py-1 xl:px-2 lg:px-1" key={i}
+                data-tip={titleCase(url.substring(1))}
+                data-background-color="#4028fb"
+                data-arrow-color="#4028fb">
                 <Link href={url}><a className="self-center relative">
                   {isNew && <span className="absolute bg-red w-1 h-1
                     rounded-full right-0 top-0" />}
@@ -89,10 +107,13 @@ const Nav: React.FC = () => {
 
           <input type="text" placeholder="Search..."
             className="bg-gray-700 rounded-sm py-1
-            px-2 text-sm text-gray-100 flex justify-end" />
+            px-2 text-sm text-gray-100 flex justify-end search-input" />
+          <img src="icons/search.svg" alt="Search"
+            className="absolute mr-2 mt-2" />
         </div>
       </div>
-  </nav>
+    </nav>
+  </div>
 }
 
 export default Nav
