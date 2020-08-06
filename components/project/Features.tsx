738be1d67782d0a5d044bddcf8a406cf6e4041e4
Justin <justinb@live.ca>
2020-08-05 23:53:17 -0400

    project next component template

--- components/project/Features.tsx
+++ components/project/Features.tsx
@@ -1,38 +1,38 @@
 const Features: React.FC<React.ReactNode> = () => {
 
-  return <div className="pt-40 pb-32 bg-gray-1000 shadow-2xl">
-    <div className="container mx-auto px-10 grid grid-cols-2 gap-6 mb-20 max-w-screen-md">
-      <div className="col-span-1">
+  return <div className="py-16 md:py-24 lg:py-40 bg-gray-1000 shadow-2xl proj-features-wrap">
+    <div className="container mx-auto px-10 grid grid-cols-2 gap-6 max-w-screen-md">
+      <div className="pl-0 sm:pl-4 md:pl-auto col-span-2 md:col-span-1">
         <h2 className="text-gray-100 text-2xl mb-10">Design &amp; UI</h2>
-        <ul className="text-white text-lg features">
+        <ul className="text-white text-lg proj-features">
           <li className="mb-6">
-            <span className="relative ml-3">Full all day support</span>
+            <span className="text-sm sm:text-base relative ml-3">Full all day support</span>
           </li>
           <li className="mb-6">
-            <span className="relative ml-3">Always open for communication</span>
+            <span className="text-sm sm:text-base relative ml-3">Always open for communication</span>
           </li>
           <li className="mb-6">
-            <span className="relative ml-3">Nice work approach</span>
+            <span className="text-sm sm:text-base relative ml-3">Nice work approach</span>
           </li>
           <li className="mb-6">
-            <span className="relative ml-3">Best field advisers ever</span>
+            <span className="text-sm sm:text-base relative ml-3">Best field advisers ever</span>
           </li>
         </ul>
       </div>
-      <div className="col-span-1">
+      <div className="pl-0 sm:pl-4 md:pl-auto md:pl-auto col-span-2 md:col-span-1">
         <h2 className="text-gray-100 text-2xl mb-10">Web &amp; Tech</h2>
-        <ul className="text-white text-lg features">
+        <ul className="text-white text-lg proj-features">
           <li className="mb-6">
-            <span className="relative ml-3">Full all day support</span>
+            <span className="text-sm sm:text-base relative ml-3">Full all day support</span>
           </li>
           <li className="mb-6">
-            <span className="relative ml-3">Always open for communication</span>
+            <span className="text-sm sm:text-base relative ml-3">Always open for communication</span>
           </li>
           <li className="mb-6">
-            <span className="relative ml-3">Nice work approach</span>
+            <span className="text-sm sm:text-base relative ml-3">Nice work approach</span>
           </li>
-          <li className="mb-6">
-            <span className="relative ml-3">Best field advisers ever</span>
+          <li className="">
+            <span className="text-sm sm:text-base relative ml-3">Best field advisers ever</span>
           </li>
         </ul>
       </div>
--- components/project/Next.tsx
+++ components/project/Next.tsx
@@ -7,18 +7,18 @@ const Next: React.FC<React.ReactNode> = () => {
 
     <Link href="#"><a>
       <div className="grid grid-cols-8 group">
-        <div className="col-span-6 group-hover:opacity-75">
+        <div className="col-span-10 md:col-span-6 group-hover:opacity-75">
           <div className="text-gray-100 text-lg">Next Project</div>
           <h3 className="text-gray-100 text-4xl">
             Short description about next project and some quick features overview
           </h3>
         </div>
-        <div className="col-span-2">
-        <div className="bg-purple group-hover:bg-dark-purple
-        group-hover:opacity-100 float-right w-20 h-20 rounded-full
-        flex align-center mt-10">
+        <div className="col-span-2 hidden md:block">
+          <div className="bg-purple group-hover:bg-dark-purple
+            group-hover:opacity-100 float-right w-20 h-20 rounded-full
+            flex align-center mt-10">
               <img src="/icons/arrow-right.svg" className="block mx-auto" />
-            </div>
+          </div>
         </div>
       </div>
     </a></Link>
