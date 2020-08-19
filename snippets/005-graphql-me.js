f537f0263eece63251bb9ff2c0cf86fbb0c12130
Justin <justinb@live.ca>
2020-08-18 21:02:24 -0400

    more posts and fix favicon

---
+++ snippets/005-graphql-me.js
@@ -0,0 +1,18 @@
+export const meOne = `
+name: Justin Brazeau
+occupation:  Frontend Developer
+country: CA
+region: Niagara
+languages:
+- english
+website: oddscenes.com
+social:
+  twitter: @oddscenes
+  github: @oddscenes
+  twitch: @oddscenes
+work:
+  linkedIn: linkedin.com/in/justinbrazeau
+  toptal: toptal.com/resume/justin-brazeau
+contact:
+  email: oddscenes@gmail.com
+`
