4d3596ea582698ef5e61b6912f1801164b630277
Justin <justinb@live.ca>
2020-08-31 20:58:03 -0400

    about v1

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -1,15 +1,25 @@
 const About: React.FC<React.ReactNode> = () => {
 
-  return <div className="px-12 py-12 md:py-40 h-auto bg-gray-900 about">
+  return <div className="px-12 py-12 md:py-40 h-auto bg-gray-900 about relative">
     <div className="container mx-auto md:px-12 lg:px-24">
 
       <div className="grid grid-cols-12 md:h-full">
         <div className="col-span-12 md:col-span-5 flex items-center">
-          <div className="bg-gray-500 w-full h-64 mb-10"></div>
+          {/* <div className="bg-gray-500 w-full h-64 mb-10"></div> */}
+          <img src="/art/about-front.png"
+            // 42
+            className="absolute bottom-0 mx-auto right-0 z-0" style={{left: '-46rem', maxWidth: '800px'}} />
         </div>
         <div className="hidden md:flex md:col-span-1"></div>
-        <div className="col-span-12 md:col-span-6 flex items-center text-white text-xl">
+        <div className="col-span-12 md:col-span-6 flex items-center text-white text-xl relative z-10">
           <div>
+            <img
+              src="/art/avatar-illustration.png"
+              className="w-24 mb-4"
+              style={{borderRadius: '1.5rem', cursor: 'crosshair'}}
+              onMouseEnter={(e) => e.target.src = '/home/profile.jpg'}
+              onMouseLeave={(e) => e.target.src = '/art/avatar-illustration.png'}
+            />
             <p className="mb-8">Hi, my name is Justin Brazeau and I’m a Canadian web designer.
             I've been designing and building websites since the early days of Geocities.
             <br /><br />Lately I'm focused on frontend development with CSS, Javascript &amp; Node. I
@@ -27,7 +37,7 @@ const About: React.FC<React.ReactNode> = () => {
               <div className="col-span-7 md:col-span-3">
               <h2 className="uppercase mb-4 md:pl-4">Tech</h2>
                 <ul className="text-gray-200 md:pl-4">
-                  <li className="text-hot-pink font-bold mb-3">Frontend Dev</li>
+                  <li className="gr-rb-dark gr-rb-dark-home mb-3">Frontend Dev</li>
                   <li className="mb-3">Backend Integration</li>
                   <li className="mb-3">DevOps &amp; Admin</li>
                 </ul>
