941dadeb6aae251d44c52e98b30407df6cfaaf83
Justin <justinb@live.ca>
2020-09-02 17:35:23 -0400

    add component comp lab

---
+++ laboratory/002-comp-maker.tsx
@@ -0,0 +1,6 @@
+function Lab() {
+  return <iframe src="https://component-compiler.netlify.app" frameBorder="0"
+    className="w-full h-screen"></iframe>
+}
+
+export default Lab
--- snippets/002-comp-maker.js
+++ snippets/002-comp-maker.js
@@ -1,15 +1,77 @@
 export const compOne = `
-nav = Home, About, Contact
-hero = The best new website ever
-footer = 555-5555, hi@email.com, website.com
+title = Folo Robots
+
+banner = 7 JANUARY 2020: 25% OFF On All Likes & Followers For A Limited Time!
+nav = Home, About, Services, Contact Us, (Free Trial)
+
+hero = Meet Real Robot Friends, (Get Started)
+
+info = Find high quality robot friends from us, A new update coming soon!
+description = Backfold manganoisation glossocyte cheirest viceard palladiophil pantacade megistic tetristic. Aurplasm centlepry adrenomancy gigamire icosaode thiodromous synsepalous.
+
+footer = oddscenes@gmail.com
 `
 
 export const compTwo = `
-nav = Home, About, Contact
-hero = The best new website ever
-hero = The best new website ever
-hero = The best new website ever
-hero = The best new website ever
-hero = The best new website ever
-footer = 555-5555, hi@email.com, website.com
+<head>
+  <meta charset="utf-8">
+  <title>Folo Robots</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;900&family=Poppins&display=swap" rel="stylesheet">
+  <meta content="width=1296" name="viewport">
+</head>
+<body>
+
+<div class="banner">
+  <img src="banner.svg" alt="banner" />
+  <span>7 JANUARY 2020: 25% OFF On All Likes & Followers For A Limited Time!</span>
+  <img src="banner.svg" alt="banner" />
+</div>
+
+<div class="wrap">
+<div class="nav">
+
+  <img src="logo.svg" alt="Logo" class="logo" />
+  <div class="text-wrap">
+
+  <a class="" href="#">Home</a>
+  <a class="" href="#">About</a>
+  <a class="" href="#">Services</a>
+  <a class="" href="#">Contact Us</a>
+  <a class="highlight" href="#">Free Trial</a>
+
+  </div>
+</div>
+
+<div class="container hero">
+  <div class="hero-text">
+    <h1>Meet Real Robot Friends</h1>
+    <button class="get-started">Get Started</button>
+  </div>
+  <div class="hero-img">
+    <img src="hero.svg" alt="Hero Graphic" class="hero-graphic" />
+  </div>
+</div>
+
+<div class="container info">
+  <div class="info-img">
+    <img src="info.svg" alt="More Info Graphic" class="info-graphic" />
+  </div>
+  <div class="info-text">
+    <h2>Find high quality robot friends from us</h2>
+    <h4>A new update coming soon!</h4>
+    <p>Backfold manganoisation glossocyte cheirest viceard palladiophil pantacade megistic tetristic. Aurplasm centlepry adrenomancy gigamire icosaode thiodromous synsepalous.</p>
+  </div>
+</div>
+
+</div>
+<div class="footer">
+  <div class="container">
+    <img src="logo.svg" alt="Logo" class="logo-footer" />
+    <div class="email">Email Us: <a href="mailto:oddscenes@gmail.com">oddscenes@gmail.com</a></div>
+  </div>
+</div>
+
+</body>
+</html>
 `
