428fe2ec1785a766fe3eb49e5b386aa0e8546571
Justin <justinb@live.ca>
2020-09-09 14:14:49 -0400

    add og meta tags

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -41,6 +41,19 @@ const Header: React.FC<React.ReactNode> = () => {
         {path}
       </title>
 
+      {/* OG Tags */}
+      <meta property="og:title" content="Odd Scenes" />
+      <meta property="og:description" content="Building the modern web with an eye for design and interaction" />
+      <meta property="og:image" content="https://oddscenes.s3.amazonaws.com/screenshot-os-website.png" />
+      <meta property="og:url" content="https://oddscenes.com" />
+      <meta property="og:site_name" content="Odd Scenes" />
+
+      <meta name="twitter:title" content="Odd Scenes" />
+      <meta name="twitter:description" content="Building the modern web with an eye for design and interaction" />
+      <meta name="twitter:image" content=" https://oddscenes.s3.amazonaws.com/screenshot-os-website.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image:alt" content="Odd Scenes" />
+
       {dark === true && <link rel="icon" href="/favicon.ico" />}
       {dark === false && <link rel="icon" href="/favicon-light.ico" />}
     </Head>
